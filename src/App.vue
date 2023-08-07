acac508c0949c1eb0e4e0cd5047b805b8de9a4fb
unknown <mengkimtang@gmail.com>
2023-08-07 08:34:56 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div class="bg-teal-500 border rounded-xl w-[80%] md:w-[40%] p-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  ">
+     <h1 class="text-center align-baseline-5">Login Form</h1>
+     <div>
+       <table class="">
+         <tbody class="text-black-600">
+         
+           <tb><span>FullName</span>
+            <input type="text" placeholder="User Name"></tb>
+           
+         
+         
+           <td><span>Password</span>
+            <input type="text" placeholder="Password"></td>
+         
+ 
+         </tbody>
+       </table>
+
+       <div class="space-x-10 my-4 text-right ">
+        <button >Login</button>
+        <button >Sign Up</button>
+      </div>
+     </div>
+
+   </div>
+ 
+ </template>
