515b325ae6679887c57db0d89e764611c30a247f
mengkim168 <mengkimtang@gmail.com>
2023-08-08 16:46:10 +0700

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex justify-center items-center min-h-screen bg-indigo-100 ">
     <div class="bg-white p-8 shadow-lg rounded-[12%] ">
-      <h2 class="text-2xl font-bold mb-4 ">Login</h2>
+      <h2 class="text-2xl font-bold mb-4 text-blue-600 ">Login</h2>
       <form @submit.prevent="submitForm">
         <div class="mb-4 ">
-          <label for="email" class="block text-gray-700">Email</label>
+          <label for="email" class="block text-gray-700 ">Email</label>
           <input
             type="email"
             id="email"
             v-model="email"
-            class="mt-1 block w-full shadow-sm h-7 p-2  border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+            class="mt-1 block w-full shadow-xl h-7 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
             required
           />
         </div>
@@ -19,7 +19,7 @@
             type="password"
             id="password"
             v-model="password"
-            class="mt-1 block w-full shadow-sm h-7 p-2 text-lg border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+            class="mt-1 block w-full shadow-xl h-7 p-2 text-lg border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
             required
           />
         </div>
