e51668540d61a3a6a7c75b155865c4c9e0db364c
unknown <mengkimtang@gmail.com>
2023-08-09 03:03:46 +0700

    update

--- src/axios/Axios.js
+++ src/axios/Axios.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { useAuthStore } from "../pinia/AuthStore";
+
+const { setAuthUser } = useAuthStore();
 const Axios = axios.create({
     baseURL: 'http://localhost:4000',
     withCredentials: true
+});
+
+axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    localStorage.clear("user");
+    setAuthUser(null);
+    return Promise.reject(error);
 });
 
 export default Axios;
