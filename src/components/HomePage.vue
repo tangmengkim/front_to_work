86dc63a7cf71669e11e88b11d5343527330d7ea2
unknown <mengkimtang@gmail.com>
2023-08-11 23:59:58 +0700

    update

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="justify-center">
+    <div class="justify-center items-center m-5">
         <p class="text-center text-lg text-cyan-600 ">Hello {{ user.username }}</p>
         <button @click="Logout" class="bg-red-600 rounded-md text-white px-4 py-2 ">Logout</button>
     </div>
